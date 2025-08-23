08dac4b9ebdc2b80b7e44cade52efd516a34e3d5
lirongyaoper <lirongyaoper@163.com>
2025-08-24 07:39:32 +0800

    update data at 20250824073932

---
+++ application/lry_admin_center/controller/admin_manage.class.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('IN_RYPHP') or exit('Access Denied');
+ryphp::load_controller('common','lry_admin_center',0);
+ryphp::load_sys_class('page','',0);
+class admin_manage extends common{
+    /**
+     * @author lirongyaoper
+     * 管理员列表
+     */
+    public function init(){
+        
+    }
+}
