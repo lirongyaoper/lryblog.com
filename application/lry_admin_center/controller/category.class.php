ad060faa5f7aa107e6ae9966ce51055e167a622f
lirongyaoper <lirongyaoper@163.com>
2025-08-26 00:33:16 +0800

    update data at 20250826003316

--- application/lry_admin_center/controller/category.class.php
+++ application/lry_admin_center/controller/category.class.php
@@ -18,6 +18,22 @@ class category extends common{
         foreach($modelinfo as $val){
             $modelarr[$val['modelid']] = $val['name'];
         }
+        //处理栏目展开/收起状态（第21-35行）
+        $category_show_status= isset($_COOKIE['category_show_status_'.self::$siteid]) ? json_decode($_COOKIE['category_show_status_'.self::$siteid],true) : array();
+        $tree_toggle = 0;
+        $childid_hide = '';
+        if($category_show_status) {
+            foreach($category_show_status as $k =>$v){
+                if($v == '1'){
+                    $childid_hide .= get_category($k,'arrchildid',true).',';
+                    $tree_toggle = 1;
+                }else{
+                    $tree_toggle = 0;
+                }
+            }
+        }
+        $arrchildid_arr = explode(',',$childid_hide);
+
     }
 
 
