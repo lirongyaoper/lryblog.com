bd853581f04d1f05266ad5e9928247b43e9a6463
lirongyaoper <lirongyaoper@163.com>
2025-11-09 22:44:18 +0800

    update data at 20251109224418

--- common/config/config.php
+++ common/config/config.php
@@ -15,7 +15,7 @@ return array(
     'db_host' => '127.0.0.1',       // 服务器地址
     'db_name' => 'rycms',		// 数据库名
     'db_user' => 'root',            // 用户名
-    'db_pwd' => '3.3.',       		// 密码
+    'db_pwd' => 'lrysql01.',       		// 密码
     'db_port' => 3306,              // 端口
     'db_charset'         => 'utf8',      // 字符集
     'db_prefix' => 'rycms_',         // 数据库表前缀
